01fe8d430abf8432261725aedc2d011a0e77fdfb
jmblanco22 <61592245+jmblanco22@users.noreply.github.com>
2021-01-08 09:39:40 -0600

    Add files via upload

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 90307094 on 9/8/20.
+//  Copyright © 2020 90307094. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+            .edgesIgnoringSafeArea(.top)
+            .frame(height: 300)
+            
+            CircleImage()
+            .offset(y: -130)
+            .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua tree national park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                    .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
